b68bd5c9e38fce5a0f65e833667c529e8c087413
sam2405663 <samg58168@gmail.com>
2026-04-08 15:10:10 -0700

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,13 @@ import express from "express";
 import dotenv from'dotenv';
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 
 dotenv.config();
+
 const app = express();
+const _dirname = path.resolve();
 
 
 
@@ -19,6 +22,15 @@ const app = express();
  // .catch(err => console.log("Conn Error:", err));
  app.use("/api/auth", authRoutes);
  app.use("/api/messages", messageRoutes);
+
+ if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+
+ };
 
 
 const PORT = process.env.PORT || 3000;
